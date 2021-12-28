a992e1f3b6612b919a7c46200ac5c3c7989fd3cc
Ryan Drachenberg <tssinvestments@gmail.com>
2021-12-28 17:30:45 -0500

    added express server & websockets

--- cyberdyneBlockChain.js
+++ cyberdyneBlockChain.js
@@ -4,23 +4,7 @@ const SHA256 = require('crypto-js/sha256');
 const CryptoJS = require('crypto-js');
 const express = require('express');
 const bodyParser = require('body-parser');
-const WebSoket = require('ws');
-
-// PORT Assignment local dev
-
-let http_port = process.env.HTTP_PORT || 3010;
-let ptp_port = process.env.HTTP_PORT || 6010;
-let startPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
-
-
-let sockets = [];
-let MessageType = {
-    QUERY_LATEST : 0,
-    QUERY_ALL : 1,
-    RESPONSE_BLOCKCHAIN : 2
-};
-
-
+const WebSocket = require('ws');
 // const Block = require('./block');
 
 
@@ -107,19 +91,17 @@ class CyberDyneChain {
 }
 
 
-
 const startCyberDyneChain = new CyberDyneChain();
 
-// startCyberDyneChain.addBlock(this.id, Date.now(), {sender: "JoMama", receiver: "Ryan of course", amount: 29000});
 setTimeout(() => {
     let i = 2;
     let amounts = 1;
 
     while(i <= 103) {
         amounts += i;
-     
+        let date = new Date().getTime() / 1000
         
-        startCyberDyneChain.addBlock(new Block(i, Date.now(), {sender: `JoMama${i}`, receiver: "Ryan of course", amount: amounts}));
+        startCyberDyneChain.addBlock(new Block(i, date, {sender: `JoMama${i}`, receiver: "Ryan of course", amount: amounts}));
     // startCyberDyneChain.addBlock(new Block(3, Date.now(), {sender: "JoMama", receiver: "Ryan of course", amount: 29000}));
         // if(i == 98) {
         //     console.log(startCyberDyneChain.getChain());
@@ -141,3 +123,87 @@ setTimeout(() => {
 // startCyberDyneChain.addBlock({sender: "Your friend", receiver: "Ryan of course again, again", amount: 10000});
 // startCyberDyneChain.addBlock({sender: "Your friend's friend", receiver: "Ry got it all! ", amount: 120000});
 console.log(JSON.stringify(startCyberDyneChain, null, 15));
+
+// PORT Assignment local dev
+
+let http_port = process.env.HTTP_PORT || 3010;
+let ptp_port = process.env.HTTP_PORT || 6010;
+let startPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+
+let sockets = [];
+let MessageType = {
+    QUERY_LATEST : 0,
+    QUERY_ALL : 1,
+    RESPONSE_BLOCKCHAIN : 2
+};
+let blockchain = startCyberDyneChain.getChain();
+// console.log(blockchain);
+let initHttpServer = () => {
+    const app = express();
+
+    app.use(bodyParser.json());
+
+    app.get('/blocks', (req, res) => {
+        res.send(JSON.stringify(blockchain));
+    });
+
+    app.post('/mine-new-block', (req, res) => {
+        let newBlock = req.body.data;
+        startCyberDyneChain.addBlock(newBlock)
+        broadcast(responseLatestMsg());
+        console.log('block addded: ', JSON.stringify(newBlock));
+        res.send();
+    });
+
+    app.get('latest-block', (req, res) => {
+        let latest =  startCyberDyneChain.getLastBlock(req);
+
+        console.log('Here is the latest transaction ---> : ', blockchain[blockchain.length -1]);
+    })
+
+    app.get('/peers', (req, res) => {
+        res.send(sockets.map(p => p._socket.remoteAddress, ':', p._socket.remotePorts));
+    });
+
+    app.post('addPeer', (req, res) => {
+        connectToPeers([req.body.peer]);
+        res.send();
+    })
+
+    app.listen(http_port, () => console.log('Skynet is alive! \nPort listening on : ', http_port));
+}
+
+const connectToPeers = (newPeers) => {
+    newPeers.forEach(e => {
+        const ws = new WebSocket(e);
+        ws.on('open', () => initConnection(ws));
+        ws.on('error', () => {
+            console.log('Connection to Skynet failed! \n Please restart T1 Specs')
+        })
+    });
+}
+var initConnection = (ws) => {
+    sockets.push(ws);
+    initMessageHandler(ws);
+    initErrorHandler(ws);
+    write(ws, queryChainLengthMsg());
+};
+var responseLatestMsg = () => ({
+    'type': MessageType.RESPONSE_BLOCKCHAIN,
+    'data': JSON.stringify([getLatestBlock()])
+});
+
+
+const write = (ws, message) => ws.send(JSON.stringify(message));
+const broadcast = (message) => sockets.forEach(socket => write(socket, message));
+
+// let generateNextBlock = (blockData) => {
+//     var previousBlock = startCyberDyneChain.getLastBlock();
+//     var nextIndex = previousBlock.id + 1;
+//     var nextTimestamp = new Date().getTime() / 1000;
+//     var nextHash = startCyberDyneChain.makeHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
+//     return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash);
+// };
+
+initHttpServer();
