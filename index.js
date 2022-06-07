b13914070b0b61c2e26bd847b2b92deaa38774b4
Ryan Drachenberg <tssinvestments@gmail.com>
2022-06-07 16:08:18 -0400

    app heroku deployment

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-const Terminator = require('./terminator');
-const Transaction = require('./transactions').Transaction
-const CyberDyneChain = require('./cyberdyneBlockChain').CyberDyneChain;
-const {createWallet, validateWallet} = require('./wallet');
+//* ONLY UTILIZED FOR TESTING
+// const Terminator = require('./terminator');
+// const Transaction = require('./transactions').Transaction
+// const CyberDyneChain = require('./cyberdyneBlockChain').CyberDyneChain;
+// const {createWallet, validateWallet} = require('./wallet');
 
-const cyberDyneSystems = new CyberDyneChain();
+// const cyberDyneSystems = new CyberDyneChain();
 
-const bobWallet = createWallet();
-const aliceWallet = createWallet();
+// const bobWallet = createWallet();
+// const aliceWallet = createWallet();
 
-console.log('Wallet validation in progress... \nWallet Validated: ', validateWallet(bobWallet.privateKey, bobWallet.publicKey));
+// console.log('Wallet validation in progress... \nWallet Validated: ', validateWallet(bobWallet.privateKey, bobWallet.publicKey));
 
-// make transactions
+// // make transactions
 
-const tx1 = new Transaction(bobWallet.publicKey, aliceWallet.publicKey, 110);
-console.log(tx1);
-tx1.signTransaction(bobWallet.keyPair);
+// const tx1 = new Transaction(bobWallet.publicKey, aliceWallet.publicKey, 110);
+// console.log(tx1);
+// tx1.signTransaction(bobWallet.keyPair);
 
-cyberDyneSystems.addTransaction(tx1);
+// cyberDyneSystems.addTransaction(tx1);
 
-console.log('mining Terminator :\n', cyberDyneSystems.this.nonce);
-cyberDyneSystems.minePendingTransactions(bobWallet.publicKey);
+// console.log('mining Terminator :\n', cyberDyneSystems.this.nonce);
+// cyberDyneSystems.minePendingTransactions(bobWallet.publicKey);
 
-const tx2 = new Transaction(bobWallet.publicKey, aliceWallet.publicKey, 190);
-tx2.signTransaction(bobWallet.keyPair);
+// const tx2 = new Transaction(bobWallet.publicKey, aliceWallet.publicKey, 190);
+// tx2.signTransaction(bobWallet.keyPair);
 
-cyberDyneSystems.addTransaction(tx2);
-console.log('mining Terminator :\n', cyberDyneSystems.this.nonce);
-cyberDyneSystems.minePendingTransactions(bobWallet.publicKey);
+// cyberDyneSystems.addTransaction(tx2);
+// console.log('mining Terminator :\n', cyberDyneSystems.this.nonce);
+// cyberDyneSystems.minePendingTransactions(bobWallet.publicKey);
 
-const tx3 = new Transaction(bobWallet.publicKey, aliceWallet.publicKey, 190);
-tx3.signTransaction(bobWallet.keyPair);
+// const tx3 = new Transaction(bobWallet.publicKey, aliceWallet.publicKey, 190);
+// tx3.signTransaction(bobWallet.keyPair);
 
-cyberDyneSystems.addTransaction(tx3);
-console.log('mining Terminator :\n', cyberDyneSystems.this.nonce);
-cyberDyneSystems.minePendingTransactions(bobWallet.publicKey);
+// cyberDyneSystems.addTransaction(tx3);
+// console.log('mining Terminator :\n', cyberDyneSystems.this.nonce);
+// cyberDyneSystems.minePendingTransactions(bobWallet.publicKey);
 
-console.log('The balance of Alice(s) account is : \n', cyberDyneSystems.getBalanceOfAddress(aliceWallet.publicKey));
+// console.log('The balance of Alice(s) account is : \n', cyberDyneSystems.getBalanceOfAddress(aliceWallet.publicKey));
 
-// validation test
+// // validation test
 
-console.log('Is the CyberDyne Chain valid? : \n', cyberDyneSystems.isValid()); // returns bool if chain is valid
+// console.log('Is the CyberDyne Chain valid? : \n', cyberDyneSystems.isValid()); // returns bool if chain is valid
 
-// cyberDyneSystems.skynet_chain[1].data[0].amount = 260;
+// // cyberDyneSystems.skynet_chain[1].data[0].amount = 260;
 
-console.log(JSON.stringify(cyberDyneSystems, null, 4)); // log whole chain fingers crossed
+// console.log(JSON.stringify(cyberDyneSystems, null, 4)); // log whole chain fingers crossed
 
