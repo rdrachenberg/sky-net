a1a76efcf94006e13bc7d265380df59f057220bc
Ryan Drachenberg <tssinvestments@gmail.com>
2022-06-08 13:36:56 -0400

    favicon edits

--- cyberdyneNode.js
+++ cyberdyneNode.js
@@ -90,6 +90,7 @@ app.use(express.static(path.resolve(__dirname, './frontend/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, './frontend/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './frontend/build', 'favicon.ico'));
 })
 // app.use(express.static(path.join(__dirname, './frontend/build')));
     
